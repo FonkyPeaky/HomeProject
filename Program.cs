a91d7f7dd74d9440ff755fc0aa9409314d022dc3
Issam Elafi <94524053+FonkyPeaky@users.noreply.github.com>
2023-03-11 13:24:56 +0100

    Home

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Device.Gpio;
+using System.Device.Gpio.Drivers;
+
+namespace HomeProject
+{
+      static class Program
+    {
+        static int Main(string[] args)
+        {
+
+            int gpioBank = 1; // todo jyjy
+            int gpioLine = 6;
+
+            // /dev/gpiochip[args[0]]
+            LibGpiodDriver gpiodDriver = new LibGpiodDriver(gpioBank);
+            GpioController gpioController = new GpioController(PinNumberingScheme.Logical, gpiodDriver);
+
+            // open gpiochip[args[0]] line args[1]
+            gpioController.OpenPin(gpioLine, PinMode.Output);
+
+            while (true)
+            {
+                gpioController.Write(gpioLine, PinValue.Low);
+                Thread.Sleep(500);
+                gpioController.Write(gpioLine, PinValue.High);
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
